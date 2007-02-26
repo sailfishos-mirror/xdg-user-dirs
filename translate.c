85558b8afbaf404b7d14a5a9d45a94cfdfc863f8
Adam Jacksson <ajax@freedesktop.org>
2007-02-26 15:51:16 +0000

    Initial revision

---
+++ translate.c
@@ -0,0 +1,14 @@
+const char *strings[] = {
+  _("Desktop"), _("desktop"),	 
+  _("Documents"), _("documents"),
+  _("Download"), _("download"), 
+  _("Movies"), _("movies"),	 
+  _("Music"), _("music"),	 
+  _("Photos"), _("photos"),	 
+  _("Pictures"), _("pictures"), 
+  _("Projects"), _("projects"), 
+  _("Public"),  _("public"),
+  _("Share"),  _("share"),
+  _("Templates"), _("templates"),
+  _("Videos"), _("videos"),   
+};
